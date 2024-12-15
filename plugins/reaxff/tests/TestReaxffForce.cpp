30e7dd38aa67fd30fbabe48caa153a81b1d691d9
Dezsoe Babai <de.babai2000@gmail.com>
2024-12-15 15:47:00 +0100

    add tests
    Signed-off-by: Dezsoe Babai <de.babai2000@gmail.com>

--- plugins/reaxff/tests/TestReaxffForce.cpp
+++ plugins/reaxff/tests/TestReaxffForce.cpp
@@ -1,31 +1,33 @@
-#include "openmm/internal/AssertionUtilities.h"
 #include "openmm/Context.h"
+#include "openmm/Platform.h"
 #include "openmm/ReaxffForce.h"
 #include "openmm/System.h"
-#include "openmm/VerletIntegrator.h"
-#include "openmm/Platform.h"
 #include "openmm/Units.h"
+#include "openmm/VerletIntegrator.h"
+#include "openmm/internal/AssertionUtilities.h"
 #include <iostream>
 #include <vector>
-
 
 using namespace OpenMM;
 using namespace std;
 
 const double TOL = 1e-5;
 
-#include <cstdlib> 
+#include <cstdlib>
 
-std::string getAbsolutePath(const std::string& relativePath) {
+std::string getAbsolutePath(const std::string &relativePath)
+{
 #ifdef _WIN32
     char absPath[_MAX_PATH];
-    if (!_fullpath(absPath, relativePath.c_str(), _MAX_PATH)) {
+    if (!_fullpath(absPath, relativePath.c_str(), _MAX_PATH))
+    {
         throw std::runtime_error("Error resolving path: " + relativePath);
     }
     return std::string(absPath);
 #else
-    char* absPath = realpath(relativePath.c_str(), nullptr);
-    if (!absPath) {
+    char *absPath = realpath(relativePath.c_str(), nullptr);
+    if (!absPath)
+    {
         throw std::runtime_error("Error resolving path: " + relativePath);
     }
     std::string result(absPath);
@@ -34,11 +36,10 @@ std::string getAbsolutePath(const std::string& relativePath) {
 #endif
 }
 
-const std::string CONTROL =  "../plugins/reaxff/tests/ffield.reaxff";
-const std::string FFIELD =  "../plugins/reaxff/tests/control";
+const std::string CONTROL = "../plugins/reaxff/tests/ffield.reaxff";
+const std::string FFIELD  = "../plugins/reaxff/tests/control";
 
-Platform& platform = Platform::getPlatformByName("Reference");
-
+Platform &platform = Platform::getPlatformByName("Reference");
 
 int validateForce()
 {
@@ -47,7 +48,7 @@ int validateForce()
     system.addParticle(16.0);
     system.addParticle(16.0);
     VerletIntegrator integrator(0.01);
-    ReaxffForce* forceField = new ReaxffForce(CONTROL, FFIELD);
+    ReaxffForce     *forceField = new ReaxffForce(CONTROL, FFIELD);
 
     forceField->addAtom(0, "O", 0.0, true);
     forceField->addAtom(1, "O", 0.0, true);
@@ -59,25 +60,28 @@ int validateForce()
 
     positions[0] = Vec3(0, 0, 0);
     positions[1] = Vec3(0, 0.116, 0);
-    
-    
+
     context.setPositions(positions);
 
-
-    State state = context.getState(State::Forces | State::Energy);
     {
-        const vector<Vec3>& forces = state.getForces();
-        const double energy = state.getPotentialEnergy();
         integrator.step(100);
+        State state = context.getState(State::Forces | State::Energy);
+        const vector<Vec3> &forces = state.getForces();
+        const double        energy = state.getPotentialEnergy();
+
+        std::cout << energy << std::endl;
     }
     return 0;
 }
 
-int main(int argc, char* argv[]) {
-    try {
+int main(int argc, char *argv[])
+{
+    try
+    {
         validateForce();
     }
-    catch(const exception& e) {
+    catch (const exception &e)
+    {
         cout << "exception: " << e.what() << endl;
         return 1;
     }
